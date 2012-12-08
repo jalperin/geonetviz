9e7c2ebbb5433dd94ec68c269b47bf2fc36bf40a
Dan Preston <dan.r.preston@gmail.com>
2012-12-08 15:52:49 -0800

    moving files

--- geonetviz/server/static/map.js
+++ geonetviz/server/static/map.js
@@ -5,12 +5,12 @@ function Map() {
 	this.selector = "#mapview > #map";
 
 	// public properties
-	this.centered = null;	
+	this.centered = null;
 	this.projection = null;
 	this.path = null;
 	this.svg = null;
 	this.g = null;
-	this.country_group = null; 
+	this.country_group = null;
 	this.arc_group = null;
 }
 
@@ -21,6 +21,13 @@ Map.prototype.init = function() {
 
 	this.path = d3.geo.path()
 	    .projection(this.projection);
+
+	this.zoom = d3.behavior.zoom()
+	    .translate(this.projection.translate())
+	    .scale(this.projection.scale())
+	    .scaleExtent([this.height, 8 * this.height])
+	    .on("zoom", this.zoommove);
+
 
 	this.svg = d3.select(this.selector).append("svg")
 	    .attr("width", this.width)
@@ -36,49 +43,60 @@ Map.prototype.init = function() {
 	    .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");
 
 	this.country_group = this.g.append("g")
-	    .attr("id", "countries");
+	    .attr("id", "countries")
+		.call(this.zoom);
 
 	this.arc_group = this.g.append("g")
-	    .attr("id", "arcs");
+	    .attr("id", "arcs")
+		.call(this.zoom);
 
 	// load the base map
-	d3.json("world-countries.json", function(countries) {
+	d3.json("/static/world-countries.json", function(countries) {
 	    map.country_group.selectAll("path")
 	      .data(countries.features)
 	    .enter().append("path")
 	      .attr("d", map.path)
 	      .attr("country_code", function(d) { return d.id; })
-	      .on("click", map.click);                      
+	      .on("click", map.click);
 	});
 }
 
 Map.prototype.loadNetwork = function() {
-	// TODO: build the URL to get_data here	
-	d3.json("collab2008.json", function(network) {    
-		// Question: I wanted to use "this.arc_group", but 
+	var themap = this;
+
+	// TODO: build the URL to get_data here
+	d3.json("/static/collab2008.json", function(network) {
+		// Question: I wanted to use "this.arc_group", but
 		// I don't understand JS' "this" keyword. help!
-            var arcs = map.arc_group.selectAll("path")
+            var arcs = themap.arc_group.selectAll("path")
 				.data(network.links);
-			
+
 			arcs
               .enter().append("path")
  			    .attr("class", "arc")
             	.attr("d", function(d) {
-					var tmp_src = map.country_group.selectAll('[country_code=' + network.nodes[d.source].id + ']');
-					var tmp_tgt = map.country_group.selectAll('[country_code=' + network.nodes[d.target].id + ']');
+					var tmp_src = themap.country_group.selectAll('[country_code=' + network.nodes[d.source].id + ']');
+					var tmp_tgt = themap.country_group.selectAll('[country_code=' + network.nodes[d.target].id + ']');
 
 					if (tmp_src[0].length && tmp_tgt[0].length ) {
 						var source_country = tmp_src.datum();
 						var target_country = tmp_tgt.datum();
 						var line = d3.svg.line();
-						return line([map.path.centroid(source_country), map.path.centroid(target_country)]);				  
-					} 
+						return line([themap.path.centroid(source_country), themap.path.centroid(target_country)]);
+					}
 	              });
-				  
+
 			arcs
 			  .exit()
-			    .remove();			    
-	        });            
+			    .remove();
+	        });
+}
+
+Map.prototype.zoommove = function () {
+	// FIXME: how to get reference to map instance in a non-global way?
+    map.projection.translate(d3.event.translate).scale(d3.event.scale);
+    map.country_group.selectAll("path").attr("d", map.path);
+	map.arc_group.selectAll("path").attr("d", map.path);
 }
 
 Map.prototype.click = function (d) {
--- geonetviz/server/static/nodelink.js
+++ geonetviz/server/static/nodelink.js
@@ -4,114 +4,65 @@
 function NodeLink() {
 	// parameters
 	this.margin = {top: 20, right: 10, bottom: 10, left: 20};
-	this.width = 400;
-	this.height = 350;
-	this.selector = "#matrixview > #matrix";
+	this.width = 960;
+	this.height = 500;
+	this.selector = "#nodelinkview > #nodelink";
 
 	// public properties
 	this.svg = null;
-	this.x = null;
-	this.z = null;
 	this.c = null;
-	this.matrix = [];
+	this.force = null;
 }
-
 
 
 NodeLink.prototype.init = function() {
-	this.x = d3.scale.ordinal().rangeBands([0, this.width]);
-	// FIXME: this requires a maximum to come from the data
-	this.z = d3.scale.linear().domain([0, 1000]).clamp(true);
-	this.c = d3.scale.category10().domain(d3.range(10));	
 	
-	this.svg = d3.select(this.selector).append("svg")
-	    .attr("width", this.width + this.margin.left + this.margin.right)
-	    .attr("height", this.height + this.margin.top + this.margin.bottom)
-	    .style("margin-left", this.margin.left + "px")
-	  .append("g")
-	    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-}
+	this.c = d3.scale.category20();
 
-NodeLink.prototype.row = function(row) {
-	var cell = d3.select(this).selectAll(".cell")
-		.data(row.links);
-	
-	cell
-	  .enter().append("rect");
-	
-	// cell
-	//   .exit().remove();
-	
-	cell
-	      .attr("class", "cell")
-	      .attr("x", function(d, i) { return matrix.x(i); })
-	      .attr("width", matrix.x.rangeBand())
-	      .attr("height", matrix.x.rangeBand())
-	      .style("fill-opacity", function(d) { return matrix.z(d.z); })
-		  .style("fill", "bluesteel");
-		  
-	      // .style("fill", function(d) { return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null; })
-// 	      .on("mouseover", mouseover)
-// 	      .on("mouseout", mouseout);
+	this.force = d3.layout.force()
+    	.charge(-120)
+   		.linkDistance(30)
+   		.size([nodelink.width, nodelink.height]);
+
+	this.svg = d3.select("body").append("svg")
+    	.attr("width", nodelink.width)
+    	.attr("height", nodelink.height);
 }
 
 NodeLink.prototype.loadNetwork = function() {
-	// TODO: build the URL to get_data here	
-	d3.json("static/collab2008.json", function(network) {    
 
-		// set the domain to each node id
-		matrix.x.domain(d3.keys(network.nodes));
-		
-		network.nodes.forEach(function(node, i) {
-			matrix.matrix[i] = d3.range(network.nodes.length).map(function(j) { return {x: j, y: i, z: 0}; });
-		});
-			
-		// Convert links to matrix; include link weight
-		network.links.forEach(function(link) {
-			matrix.matrix[link.source][link.target].z += link.weight;
-			matrix.matrix[link.target][link.source].z += link.weight;
-			// nodes[link.source].count += link.weight;
-			// nodes[link.target].count += link.weight;
-		});		
-		
-		// add in rows that are needed
-		// NOTE: this implementation does not allow for client-side filtering
-		// it depends on the node indexes for the position of the cells
-		// any deletions will caude indexes to not align with right row
-		var rowSelection = matrix.svg.selectAll(".row")
-			.data(network.nodes.map(function(node, i) { return {index: i, id: node.id, links: matrix.matrix[i]}; }))
-		  .enter().append("g")
-			.attr("class", "row")
-			.attr("transform", function(d, i) { return "translate(0," + matrix.x(i) + ")"; })
-			.each(matrix.row);
+		d3.json("/static/miserables.json", function(error, graph) {
+		  force
+			  .nodes(graph.nodes)
+			  .links(graph.links)
+			  .start();
 
-		// remove columns that are not part of the filter
-		var columnSelection = matrix.svg.selectAll(".column")
-			.data(network.nodes.map(function(node, i) { return {index: i, id: node.id, links: matrix.matrix[i]}; }))
-		  .enter().append("g")
-			.attr("class", "column")
-			.attr("transform", function(d, i) { return "translate(" + matrix.x(i) + ")rotate(-90)"; });
-			
+		  var link = svg.selectAll("line.link")
+			  .data(graph.links)
+			.enter().append("line")
+			  .attr("class", "link")
+			  .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-		rowSelection.append("line")
-			.attr("x2", matrix.width);
+		  var node = svg.selectAll("circle.node")
+			  .data(graph.nodes)
+			.enter().append("circle")
+			  .attr("class", "node")
+			  .attr("r", 5)
+			  .style("fill", function(d) { return color(d.group); })
+			  .call(force.drag);
 
-		columnSelection.append("line")
-			.attr("x1", -matrix.width);			
+		  node.append("title")
+			  .text(function(d) { return d.name; });
 
-		rowSelection.append("text")
-			.attr("x", 10)
-			.attr("y", matrix.x.rangeBand() / 4)
-			.attr("dy", ".01em")
-			.attr("text-anchor", "end")
-			.text(function(d) { return d.id });
+		  force.on("tick", function() {
+			link.attr("x1", function(d) { return d.source.x; })
+				.attr("y1", function(d) { return d.source.y; })
+				.attr("x2", function(d) { return d.target.x; })
+				.attr("y2", function(d) { return d.target.y; });
 
-		columnSelection.append("text")
-			.attr("x", 6)
-			.attr("y", matrix.x.rangeBand() / 4)
-			.attr("dy", ".01em")
-			.attr("text-anchor", "start")
-			.text(function(d) { return d.id });
-	});          
+			node.attr("cx", function(d) { return d.x; })
+				.attr("cy", function(d) { return d.y; });
+		  });
+		});	
 }
 
