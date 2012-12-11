51b6d1bf2131bdc024f45f9299ac7d98c0164412
Dan Preston <dan.r.preston@gmail.com>
2012-12-10 16:00:36 -0800

    stats graph does filtering correctly

--- server/static/stats.js
+++ server/static/stats.js
@@ -8,20 +8,62 @@ function Stats() {
 	// public properties
 	this.svg = null;
 	this.data = null;
+    this.vars = null;
 }
 
 Stats.prototype.init = function() {
-	/*this.x = d3.scale.ordinal().rangeBands([0, this.width]);
-	// FIXME: this requires a maximum to come from the data
-	this.z = d3.scale.linear().domain([0, 1000]).clamp(true);
-	this.c = d3.scale.category10().domain(d3.range(10));
 
-	this.svg = d3.select(this.selector).append("svg")
-	    .attr("width", this.width + this.margin.left + this.margin.right)
-	    .attr("height", this.height + this.margin.top + this.margin.bottom)
-	    .style("margin-left", this.margin.left + "px")
-	  .append("g")
-	    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");*/
+    var x = d3.scale.ordinal()
+        .rangeBands([0, this.width], .2);
+
+    var y = d3.scale.linear()
+        .range([this.height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    this.svg = d3.select(this.selector).append("svg")
+        .attr("width", this.width + this.margin.left + this.margin.right)
+        .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+    x.domain([1, 2, 3, 4, 5])
+    y.domain([0, 100])
+
+      this.svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(xAxis);
+
+  this.svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -40)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+    var height=this.height;
+    this.bars = this.svg.selectAll(".bar").data([1, 2, 3, 4, 5]);
+
+    this.bars
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return 5; })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return 10; })
+        .attr("height", function(d) { return height - 10; });
+
+    //this.bars.exit().remove();
+
 }
 
 Stats.prototype.loadNetwork = function(network) {
@@ -29,19 +71,13 @@ Stats.prototype.loadNetwork = function(network) {
 
     var data = this.data.extra_graphs[0].data;
 
-    var width = this.width;
-    var height = this.height;
-    var margin = this.margin;
-
-    //var formatPercent = d3.format(".0%");
-
     var xdomain = data.map(function(d) { return d.x; });
     var x = d3.scale.ordinal()
         .domain(xdomain)
-        .rangeBands([0, width], .2);
+        .rangeBands([0, this.width], .2);
 
     var y = d3.scale.linear()
-        .range([height, 0]);
+        .range([this.height, 0]);
 
     var numticks = Math.round(this.width / 100)
     var maxval = d3.max(data, function(d) { return d.x; })
@@ -57,38 +93,30 @@ Stats.prototype.loadNetwork = function(network) {
         .orient("left");
         //.tickFormat(formatPercent);
 
-    this.svg = d3.select(this.selector).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    x.domain(data.map(function(d) { return d.x; }));
+    y.domain([0, d3.max(data, function(d) { return d.y; })]);
 
-  x.domain(data.map(function(d) { return d.x; }));
-  y.domain([0, d3.max(data, function(d) { return d.y; })]);
+    this.svg.select(".x.axis").call(xAxis);
+    this.svg.select(".y.axis").call(yAxis);
 
-  this.svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    var height=this.height;
+    this.bars = this.svg.selectAll('.bar').data(data)
+    this.bars
+        .enter().append('rect')
+        .attr('class', 'bar')
 
-  this.svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -40)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Frequency");
+    this.bars
+        .transition().duration(2000).delay(100)
+        .attr("x", function(d) { return Math.round(x(d.x)); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.y); })
+        .attr("height", function(d) { return height - y(d.y); });
 
-  this.svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return Math.round(x(d.x)); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.y); })
-      .attr("height", function(d) { return height - y(d.y); });
+    this.bars
+        .exit()
+        .transition().duration(750)
+        .style("fill-opacity", 1e-6)
+        .remove();
 
 }
 
